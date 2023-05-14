8a6589bf4c261c59a35382785e8abb49edcf58c5
JuanR547 <95152166+JuanR547@users.noreply.github.com>
2023-05-14 18:56:57 -0300

    login e implementacion de base de datos

--- ProyectoIntegrador_ReynosoJuan/portfolio_BackEnd/src/main/java/com/portfolioJR/jr/Controller/PersonaController.java
+++ ProyectoIntegrador_ReynosoJuan/portfolio_BackEnd/src/main/java/com/portfolioJR/jr/Controller/PersonaController.java
@@ -4,6 +4,7 @@ import com.portfolioJR.jr.Entity.Persona;
 import com.portfolioJR.jr.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,10 +15,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
-    @GetMapping("personas/traer")
+    @GetMapping("persona/traer")
     public List<Persona> getPersona() {
         return ipersonaService.getPersona();
     }
@@ -28,13 +30,13 @@ public class PersonaController {
         return "La persona fue creada correctamente";
     }
     
-    @DeleteMapping("/persona/borrar/(id)")
+    @DeleteMapping("/persona/borrar/{id}")
     public String deletePersona (@PathVariable Long id) {
         ipersonaService.deletePersona(id);
         return "La persona fue eliminada correctamente";
     }
     
-    @PutMapping ("/personas/editar/(id)")
+    @PutMapping ("/persona/editar/{id}")
     public Persona editPersona(@PathVariable Long id, 
                                 @RequestParam("nombre") String nuevoNombre,
                                 @RequestParam("apellido") String nuevoApellido,
--- ProyectoIntegrador_ReynosoJuan/portfolio_FrontEnd/src/app/app.module.ts
+++ ProyectoIntegrador_ReynosoJuan/portfolio_FrontEnd/src/app/app.module.ts
@@ -11,6 +11,9 @@ import { EducacionComponent } from './components/educacion/educacion.component';
 import { HabilidadesComponent } from './components/habilidades/habilidades.component';
 import { ProyectosComponent } from './components/proyectos/proyectos.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
@@ -22,11 +25,14 @@ import { FooterComponent } from './components/footer/footer.component';
     EducacionComponent,
     HabilidadesComponent,
     ProyectosComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
